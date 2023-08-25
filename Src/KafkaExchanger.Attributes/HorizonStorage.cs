f6e3fec81c9f55eb9fb0126bffb97efddbfaa2ff
Vyacheslav <softstonedevelop@gmail.com>
2023-08-25 18:22:06 +0300

    feat:
    + Find
    + CanFree
    + Clear
    
    https://github.com/SoftStoneDevelop/KafkaExchanger/issues/14

---
+++ Src/KafkaExchanger.Attributes/HorizonStorage.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace KafkaExchanger
+{
+    public class HorizonStorage
+    {
+        private long[] _data;
+        private int _size;
+
+        public HorizonStorage()
+        {
+            _data = new long[10];
+            _size = 0;
+        }
+
+        public int Size
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _size;
+        }
+
+        /// <summary>
+        /// Add new horizon info
+        /// </summary>
+        /// <returns>Index of new element in storage</returns>
+        /// <exception cref="Exception">If iitem already contains</exception>
+        public int Add(long item)
+        {
+            if (_size >= _data.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            int i = _size - 1;
+            //right shift
+            for (; i >= 0; i--)
+            {
+                if (_data[i] == item)
+                {
+                    throw new Exception("New element already contains, HorizonStorage is corrupted");
+                }
+
+                if (_data[i] > item)
+                {
+                    break;
+                }
+
+                _data[i + 1] = _data[i];
+            }
+
+            _data[i + 1] = item;
+            _size++;
+
+            return i + 1;
+        }
+
+        public int Find(long horizonId)
+        {
+            int lo = 0;
+            int hi = _size - 1;
+            while (lo <= hi)
+            {
+                int i = lo + ((hi - lo) >> 1);
+                int order = _data[i].CompareTo(horizonId);
+
+                if (order == 0)
+                {
+                    return i;
+                }
+
+                if (order < 0)
+                {
+                    hi = i - 1;
+                }
+                else
+                {
+                    lo = i + 1;
+                }
+            }
+
+            throw new Exception("HorizonId not found");
+        }
+
+        public int CanFree(int index)
+        {
+            return _size - index;
+        }
+
+        public long Clear(int index)
+        {
+            var horizonId = _data[index];
+            _size = index;
+
+            return horizonId;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void IncreaseCapacity()
+        {
+            var oldData = _data;
+            _data = new long[oldData.Length * 2];
+
+            oldData.CopyTo(_data, 0);
+        }
+
+        public ref long this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                if (index >= _size)
+                    throw new IndexOutOfRangeException();
+
+                return ref _data[index];
+            }
+        }
+
+        public HorizonStorage.Enumerator GetEnumerator() => new HorizonStorage.Enumerator(this);
+
+        public ref struct Enumerator
+        {
+            private readonly HorizonStorage _array;
+            private int _index;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Enumerator(HorizonStorage array)
+            {
+                _array = array;
+                _index = _array.Size;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                int index = _index - 1;
+                if (index > 0)
+                {
+                    _index = index;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public ref long Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => ref _array[_index];
+            }
+        }
+    }
+}
--- Src/Tests/Tests.cs
+++ Src/Tests/Tests.cs
@@ -9,7 +9,7 @@ namespace Tests
         [Test]
         public void Add()
         {
-            var array = new UniqueSortedArray();
+            var array = new HorizonStorage();
             for (long j = 0; j < 5000; j++)
             {
                 array.Add(j);
@@ -26,10 +26,10 @@ namespace Tests
         [Test]
         public void AddNotUnique()
         {
-            for (int i = 0; i < 5000; i++)
+            for (int i = 0; i < 1000; i++)
             {
-                var array = new UniqueSortedArray();
-                for (int j = 0; j < 5000; j++)
+                var array = new HorizonStorage();
+                for (int j = 0; j < 1000; j++)
                 {
                     array.Add(j);
                 }
@@ -43,13 +43,71 @@ namespace Tests
                 catch(Exception e)
                 {
                     pass = true;
-                    Assert.That(e.Message, Is.EqualTo("New element already contains in array, array is corrupted"));
+                    Assert.That(e.Message, Is.EqualTo("New element already contains, HorizonStorage is corrupted"));
                 }
 
                 if(!pass)
                 {
                     Assert.Fail($"Not unique value({i}) not throw Exception");
                 }
+            }
+        }
+
+        [Test]
+        public void ClearAfter()
+        {
+            var array = new HorizonStorage();
+            for (long j = 0; j < 1000; j++)
+            {
+                array.Add(j);
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var indx = array.Find(i);
+                array.Clear(indx);
+                Assert.That(array.Size, Is.EqualTo(999 - i));
+            }
+
+            array.Clear(0);
+            Assert.That(array.Size, Is.EqualTo(0));
+
+            array.Clear(0);
+            Assert.That(array.Size, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Find()
+        {
+            var items = 1000;
+            var array = new HorizonStorage();
+            for (long j = 0; j < items; j++)
+            {
+                array.Add(j);
+            }
+
+            var value = items - 1;
+            for (long j = 0; j < items; j++)
+            {
+                var index = array.Find(value--);
+                Assert.That(index, Is.EqualTo(j));
+            }
+        }
+
+        [Test]
+        public void CanFree()
+        {
+            var array = new HorizonStorage();
+            for (long j = 0; j < 1000; j++)
+            {
+                array.Add(j);
+            }
+
+            for (long j = 0; j < 1000; j++)
+            {
+                var index = array.Find(j);
+                var canFreeCount = array.CanFree(index);
+                Assert.That(canFreeCount, Is.EqualTo(j + 1));
             }
         }
     }
